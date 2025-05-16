7ce29fc1d6e28d1e2872bd340b1325825564dc9c
Ayomide <ayomidedans@gmail.com>
2025-05-16 22:01:23 +0100

    added fonts including folder structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,38 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Regular": require('../assets/fonts/Poppins-Regular.ttf'),
+    "Poppins-Medium": require('../assets/fonts/Poppins-Medium.ttf'),
+    "Poppins-SemiBold": require('../assets/fonts/Poppins-SemiBold.ttf'),
+    "Poppins-Bold": require('../assets/fonts/Poppins-Bold.ttf'),
+    "Poppins-ExtraBold": require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    "Poppins-Black": require('../assets/fonts/Poppins-Black.ttf'),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </Stack>
   );
 }
