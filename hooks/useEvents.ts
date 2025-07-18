ccad1f1237462201ef223a8b8aff6dfe865f76b4
Ayomide <ayomidedans@gmail.com>
2025-07-18 02:53:10 +0100

    created useEvents hook

--- hooks/useEvents.ts
+++ hooks/useEvents.ts
@@ -1,33 +1,14 @@
+import { EventInsert } from "@/types";
+import { supabase } from "@/utils/supabase";
 import { useQuery } from "@tanstack/react-query";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const anonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const useEvents = () => {
-  return useQuery({
+export function useEvents() {
+  return useQuery<EventInsert[], Error>({
     queryKey: ["events"],
     queryFn: async () => {
-      // Trigger seed
-      await fetch(`${supabaseUrl}/functions/v1/seed-events`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${anonKey}`,
-        },
-      });
-
-      // Fetch all events from the `events` table
-      const res = await fetch(`${supabaseUrl}/rest/v1/events?select=*`, {
-        headers: {
-          apikey: anonKey,
-          Authorization: `Bearer ${anonKey}`,
-        },
-      });
-
-      if (!res.ok) throw new Error("Failed to fetch events");
-
-      const data = await res.json();
-      return data;
+      const { data, error } = await supabase.from("events").select("*");
+      if (error) throw error;
+      return data || [];
     },
-    staleTime: 1000 * 60 * 5,
   });
-};
+}
