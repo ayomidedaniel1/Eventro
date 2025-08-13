8b061b972a2dfd6ddf23cad0f73b88ddc5506757
Ayomide <ayomidedans@gmail.com>
2025-08-13 17:48:34 +0100

    added tickets and saved navigation

--- app/(tabs)/events/_layout.tsx
+++ app/(tabs)/events/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack } from 'expo-router';
 export default function EventLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="[id]/detail" options={{ headerShown: false }} />
       <Stack.Screen name="[id]/chat" options={{ headerShown: false }} />
     </Stack>
