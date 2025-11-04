d7880ea0b424ed1d89011c258ae8a9dd07dd55e6
Ayomide <ayomidedans@gmail.com>
2025-11-04 02:03:43 +0100

    fixed react native paper type declaration

---
+++ types/react-native-paper.d.ts
@@ -0,0 +1,7 @@
+// This file is a manual type declaration (a shim) required because the
+// 'react-native-paper' package bundles its own types, but the TypeScript
+// compiler sometimes fails to automatically resolve the module's declaration.
+
+// This declaration tells TypeScript that the module exists, silencing the
+// 'Could not find a declaration file for module...' error.
+declare module 'react-native-paper';
